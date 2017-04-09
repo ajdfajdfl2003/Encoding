139ee5660d00cd3942040927d28993994f0d2371
Angus <ajdfajdfl2003@gmail.com>
2017-04-09 10:16:04 +0800

    Add Test_我是測試員私はテスターです我是测试员_to_Decode Testcase

--- EncodingTests/Base64Test.cs
+++ EncodingTests/Base64Test.cs
@@ -7,6 +7,18 @@ namespace EncodingTests
     public class Base64Test
     {
         [TestMethod]
+        public void Test_我是測試員私はテスターです我是测试员_to_Decode()
+        {
+            var target = new Base64();
+
+            var plaintext = "我是測試員私はテスターです我是测试员";
+            var encode = target.Encode(plaintext);
+            var actual = target.Decode(encode);
+
+            Assert.AreEqual(plaintext, actual);
+        }
+
+        [TestMethod]
         public void Test_80b898c6aec057YaccAL4QgXKORW_Encode_to_Decode()
         {
             var target = new Base64();
