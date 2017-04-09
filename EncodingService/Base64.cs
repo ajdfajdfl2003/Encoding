83540949062fd63aff2af010491c361c35ff8425
Angus <ajdfajdfl2003@gmail.com>
2017-04-09 10:08:51 +0800

    Add Test_80b898c6aec057YaccAL4QgXKORW_Encode_to_Decode TestCase

---
+++ EncodingService/Base64.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingService
+{
+    public class Base64
+    {
+        public string Encode(string plaintext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string encode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EncodingTests/Base64Test.cs
@@ -0,0 +1,21 @@
+﻿using EncodingService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EncodingTests
+{
+    [TestClass]
+    public class Base64Test
+    {
+        [TestMethod]
+        public void Test_80b898c6aec057YaccAL4QgXKORW_Encode_to_Decode()
+        {
+            var target = new Base64();
+
+            var plaintext = "80b898c6aec057YaccAL4QgXKORW";
+            var encode = target.Encode(plaintext);
+            var actual = target.Decode(encode);
+
+            Assert.AreEqual(plaintext, actual);
+        }
+    }
+}
